60a0a394e00b260838ac6227fabf5b44f6183870
Rasmus Torell <rasmustorell@AirsomtorRasmus.lan>
2023-12-04 15:33:08 +0100

    Klar!

--- src/main/java/Day4/Day4Part2.java
+++ src/main/java/Day4/Day4Part2.java
@@ -12,16 +12,14 @@ public class Day4Part2 {
 
     public static void main(String[] args) {
 
-        Path path = Paths.get("src/main/java/Day4/TestdataDay4");
+        Path path = Paths.get("src/main/java/Day4/Day4.txt");
         String line;
         double totalSum = 0;
-        HashMap<HashMap<String, Double>, Integer> mapOfMaps = new HashMap<>();
-        List<HashMap<Double, Double>> listOfMaps = new ArrayList<>();
+        List<Double[]> listOfWinningGames = new ArrayList<>();
         try (BufferedReader bufferedReader = Files.newBufferedReader(path)) {
 
             while ((line = bufferedReader.readLine()) != null) {
                 HashMap<Double, Double> mapOfNumbers = new HashMap<>();
-                String matchName = line.substring(0, line.indexOf(":"));
                 String matchNameRemoved = line.substring(line.indexOf(":") + 1);
                 String[] allNumber = matchNameRemoved.split("\\|");
                 String[] winNumber = allNumber[0].split(" ");
@@ -49,9 +47,10 @@ public class Day4Part2 {
                 for (double value : mapOfNumbers.values()) {
                     matchingCards += value;
                 }
-                HashMap<Double, Double> currentGame = new HashMap<>();
-                currentGame.put(1.0, matchingCards);
-                listOfMaps.add(currentGame);
+                Double[] currentGame = new Double[2];
+                currentGame[0] = 1.0;
+                currentGame[1] = matchingCards;
+                listOfWinningGames.add(currentGame);
 
 
             }
@@ -59,81 +58,42 @@ public class Day4Part2 {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        int totalSize = 0;
-        for (HashMap<Double, Double> map : listOfMaps) {
-            totalSize += map.size();
-        }
 
-        System.out.println("Next loop");
-        List<Double> processedList = new ArrayList<>();
-        double[][] arrayOfWinningCards = new double[totalSize][2];
-        int i = 0;
-        for (HashMap<Double, Double> map : listOfMaps) {
-            for (Map.Entry<Double, Double> entry : map.entrySet()) {
-                arrayOfWinningCards[i][0] = entry.getKey();
-                arrayOfWinningCards[i][1] = entry.getValue();
-                i++;
+        for (Double[] games : listOfWinningGames) {
+            for (double d : games) {
+                System.out.print(d + " ");
             }
-
+            System.out.println("----");
         }
 
-        for (int j = 0; j <arrayOfWinningCards.length ; j++) {
-            for (int k = 0; k <arrayOfWinningCards[j].length ; k++) {
-                System.out.println(arrayOfWinningCards[i][j]);
-            }
+        for (int i = 0; i < listOfWinningGames.size(); i++) {
+            double copyCards = listOfWinningGames.get(i)[1];
+            System.out.println("copy card: " + copyCards);
+            double amountOfCards = listOfWinningGames.get(i)[0];
+            System.out.println("Amount of cards: " + amountOfCards);
+            for (int j = 0; j < amountOfCards; j++) {
 
-        }
-
-/*
-        for (HashMap<Double, Double> map : listOfMaps) {
-            for (Map.Entry<Double, Double> entry : map.entrySet()) {
-                double key = entry.getKey();
-                double value = entry.getValue();
-                System.out.println(key + " " + value);
-            }
-        }
-        for (int i = 0; i < listOfMaps.size(); i++) {
-            HashMap<Double, Double> currentMap = listOfMaps.get(i);
-            for (double key : currentMap.keySet()) {
-                processedList.add(key);
-                System.out.println("------------");
-                double points = currentMap.get(key);
-                totalSum += points;
-                System.out.println(points);
-
-                for (int j = i + 1; points > 0 && j < listOfMaps.size(); j++) {
-                    HashMap<Double, Double> nextMap = listOfMaps.get(j);
-                    for (double nextKey : nextMap.keySet()) {
-                        double nextQuantity = nextMap.get(key);
-                        double pointsToDistribute = Math.min(points, 1);
-                        nextMap.put(nextKey,nextQuantity + pointsToDistribute);
-                        processedList.add(nextKey);
-                        points -= pointsToDistribute;
-
-                        if (points <= 0) {
-                            break; // Break if all points from the current card are distributed
-                        }
+                for (int k = i; k < i+copyCards; k++) {
+                    listOfWinningGames.get(k+1)[0] += 1.0;
 
 
-                    }
                 }
             }
+
+
         }
 
-        Collections.sort(processedList);
+        System.out.println("+++++++++++++++++++++");
 
-        for (Double s : processedList) {
-            System.out.println(s);
+        for (Double[] games : listOfWinningGames) {
+            totalSum += games[0];
+            System.out.println("----");
         }
-        System.out.println(processedList.size());
+
+        System.out.println(totalSum);
 
 
- */
     }
-
-
-
-
 
 
     public static double countGameScore(double powerOf) {
